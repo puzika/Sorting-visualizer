b86d834542cee8295eccfceceab7d7b5487862bf
puzika <tolibjonlatipovk@gmail.com>
2023-12-04 16:09:21 +0700

    Added hash clean

---
+++ src/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+import './styles/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name][contenthash].js',
+      clean: true,
    },
 
    devtool: 'source-map',
