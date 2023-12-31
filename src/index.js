122980a1ea77e50ea56db08869b78d760a45e294
puzika <tolibjonlatipovk@gmail.com>
2023-12-31 18:28:31 +0700

    change array on input & generate

--- src/index.js
+++ src/index.js
@@ -7,6 +7,30 @@ const algs = document.querySelector('.drop-down__list');
 const sizeControl = document.querySelector('.size__range');
 const labelSize = document.querySelector('.array__size');
 const labelMethod = document.querySelector('.array__method');
+const generateButton = document.querySelector('.btn--generate');
+const sortButton = document.querySelector('.btn--sort');
+
+//ALGORITHMS
+
+const algorithms = {
+   selectionsort: function (arr) {
+      for (let i = 0; i < arr.length - 1; i++) {
+         let minIndex = i;
+
+         for (let j = i + 1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) minIndex = j;
+         }
+
+         [arr[minIndex], arr[i]];
+      }
+   },
+
+   heapsort: function (arr) {
+
+   }
+};
+
+//VIEW
 
 function getStyle(elem, style) {
    const styles = window.getComputedStyle(elem);
@@ -22,10 +46,10 @@ function genRandomNum(min, max) {
 
 function genRandomArr(size) {
    const arr = [];
-   const minHeight = arrayHeight / size;
+   const minHeight = 10;
 
    for (let i = 0; i < size; i++) {
-      const height = minHeight * (1 + genRandomNum(0, size - 1));
+      const height = genRandomNum(minHeight, arrayHeight);
       arr.push(height);
    }
 
@@ -33,17 +57,26 @@ function genRandomArr(size) {
 }
 
 function renderArr(arr) {
+   array.innerHTML = '';
+
    for (let i = 0; i < arr.length; i++) {
       const markup = `
-         <span style="height: ${arr[i]}px;" class="array__bar"></span>
+         <span style="height: ${arr[i]}px;" class="array__bar ${i === 0 ? 'array__bar--current' : ''}"></span>
       `;
 
       array.insertAdjacentHTML('beforeend', markup);
    }
 }
 
+function createArr(size) {
+   const arr = genRandomArr(size);
+   renderArr(arr);
+}
+
 sizeControl.addEventListener('input', function () {
    labelSize.textContent = sizeControl.value;
+
+   createArr(sizeControl.value);
 });
 
 algs.addEventListener('click', function (e) {
@@ -54,5 +87,14 @@ algs.addEventListener('click', function (e) {
    labelMethod.textContent = target.textContent;
 });
 
-const arr = genRandomArr(50);
-renderArr(arr);
+generateButton.addEventListener('click', function () {
+   createArr(sizeControl.value);
+});
+
+sortButton.addEventListener('click', function () {
+   const alg = labelMethod.textContent.toLowerCase().split(' ').join('');
+
+   algorithms[alg]();
+});
+
+createArr(sizeControl.value);
